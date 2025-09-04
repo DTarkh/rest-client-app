2310216061dad6fc43c6635686a2fd4d185cb7db
David Tarkhnishvili <datotarkh@gmail.com>
2025-09-04 22:53:37 +0400

    feat: combine hero, features, cta, and footer into homepage

--- src/pages-slice/home-page/ui/HomePage.tsx
+++ src/pages-slice/home-page/ui/HomePage.tsx
@@ -1,3 +1,19 @@
+import { AppHeader } from '@/src/widgets/app-header';
+import { CallToAction } from '@/src/widgets/call-to-action';
+import { Features } from '@/src/widgets/features';
+import { Footer } from '@/src/widgets/footer';
+import { Hero } from '@/src/widgets/hero';
+
 export function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <AppHeader />
+      <main className='min-h-[60vh] flex flex-col items-center justify-center gap-4'>
+        <Hero />
+        <Features />
+        <CallToAction />
+      </main>
+      <Footer />
+    </>
+  );
 }
