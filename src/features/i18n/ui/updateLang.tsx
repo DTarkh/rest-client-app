646cf62ee691ea321e2d3624663c34bf29ae87aa
Alina <lnrzhkv3367337@gmail.com>
2025-09-05 15:38:06 +0300

    feat: add i18n functionality to change the language of the application

---
+++ src/features/i18n/ui/updateLang.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { clsx } from '../../../shared/lib/clsx';
+import { UiSelect } from '../../../shared/ui/UiSelect/UiSelect';
+import { Lang, useLang } from '../model/lang.store';
+
+type LangOption = {
+  id: Lang;
+  label: string;
+};
+
+const langOptions: LangOption[] = [
+  { id: 'en', label: 'En' },
+  { id: 'ru', label: 'Ru' },
+];
+
+export function UpdateLang({ className }: { className?: string }) {
+  const { lang, setLang } = useLang();
+
+  const langOption = langOptions.find(option => option.id === lang);
+  const onChangeLang = (lang: LangOption) => {
+    setLang(lang.id);
+  };
+
+  return (
+    <UiSelect
+      className={clsx(className)}
+      widthClassName='w-[80px]'
+      options={langOptions}
+      value={langOption}
+      onChange={onChangeLang}
+      getLabel={option => option.label}
+    />
+  );
+}
