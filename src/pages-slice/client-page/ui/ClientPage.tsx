254c2a419653f14ef3cf36167017d30ff9b7ecba
Alina <lnrzhkv3367337@gmail.com>
2025-09-15 20:14:23 +0300

    feat: intergate dynamic REST client page with support for decoding requests from URLs, add localization

--- src/pages-slice/client-page/ui/ClientPage.tsx
+++ src/pages-slice/client-page/ui/ClientPage.tsx
@@ -1,3 +1,24 @@
+import { RequestBuilder } from '@/src/widgets/request-builder';
+import { ResponseViewer } from '@/src/widgets/response-viewer';
+import { useI18n } from '../model/i18n';
+
 export function ClientPage() {
-  return <div className='min-h-screen pt-20 md:pl-64'>ClientPage</div>;
+  const { t } = useI18n();
+
+  return (
+    <div className='p-4'>
+      <div className='min-h-screen pt-20 md:pl-64'>
+        <h1 className='text-gray-600 mt-2 mb-2'>{t('pageTitle')}</h1>
+
+        <div className='flex flex-col gap-8 self-center sm:w-full'>
+          <div>
+            <RequestBuilder />
+          </div>
+          <div>
+            <ResponseViewer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
