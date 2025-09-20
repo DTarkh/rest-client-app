4306a7efe57be16d9529078e295577a02ed4f141
David Tarkhnishvili <datotarkh@gmail.com>
2025-09-20 22:35:47 +0400

    style: layout adjustment on client and variables page

--- src/pages-slice/variables-page/ui/VariablesPage.tsx
+++ src/pages-slice/variables-page/ui/VariablesPage.tsx
@@ -13,15 +13,17 @@ export function VariablesPage() {
   const { t } = useI18n();
 
   return (
-    <div className='min-h-screen pt-20 md:pl-64'>
-      <div className='container mx-auto py-8'>
-        <div className='max-w-7xl mx-auto'>
-          <div className='mb-8'>
-            <h1 className='text-3xl font-bold text-gray-900 mb-4 pl-1.5'>{t('title')}</h1>
-            <p className='text-gray-600 mt-2 pl-1.5'>{t('helpText')}</p>
-          </div>
+    <div className='p-8'>
+      <div className='min-h-screen pt-20 md:pl-64'>
+        <div className='container mx-auto py-8'>
+          <div className='pb-25'>
+            <div className='mb-8'>
+              <h1 className='text-3xl font-bold text-gray-900 mb-4 pl-1.5'>{t('title')}</h1>
+              <p className='text-gray-600 mt-2 pl-1.5'>{t('helpText')}</p>
+            </div>
 
-          <VariablesManager />
+            <VariablesManager />
+          </div>
         </div>
       </div>
     </div>
