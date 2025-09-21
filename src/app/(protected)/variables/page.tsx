ff5868c61f890cf235b9c7ed61acad536ea9e83c
David Tarkhnishvili <datotarkh@gmail.com>
2025-09-21 08:23:02 +0400

    feat: enable lazy loading for variables and client pages

--- src/app/(protected)/variables/page.tsx
+++ src/app/(protected)/variables/page.tsx
@@ -1,9 +1,13 @@
-import { VariablesPage } from '@/src/pages-slice/variables-page';
+import Spinner from '@/src/shared/ui/spinner';
+import dynamic from 'next/dynamic';
+
+const VariablesPage = dynamic(
+  () => import('@/src/pages-slice/variables-page').then(m => m.VariablesPage),
+  {
+    loading: () => <Spinner />,
+  },
+);
 
 export default function Variables() {
-  return (
-    <>
-      <VariablesPage />
-    </>
-  );
+  return <VariablesPage />;
 }
